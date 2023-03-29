f1a379ceda9596ca05cfeafa51d8571d85858560
Julien Doiron <judoiron003@rogers.com>
2023-03-29 15:07:40 -0300

    Adjusted Server a small bit

--- server/src/server.js
+++ server/src/server.js
@@ -25,22 +25,18 @@ io.on("connection", (socket) => {
     const { user } = JSON.parse(message); // deserialization
     newUser(user, socket, io);
   });
-  
-  socket.on("chatMessage", chatMessage => {
-    const data = JSON.parse(chatMessage);
-    
-// not secure    
-    // io.sockets.emit('chatMessageBroadcast', JSON.stringify({
-    //   chatMessage: data
-    // }))
-    io.sockets.emit('chatMessageBroadcast', JSON.stringify({
-      chatMessage : {user: data.user, msg: xss(data.msg)}
+
+  socket.on("canvasDraw", canvasData => {
+    const data = JSON.parse(canvasData);
+
+    io.sockets.emit('broadcastCanvasValues', JSON.stringify({
+      chatMessage: { user: data.user, msg: xss(data.msg) }
     }))
   })
 
-  socket.on('disconnect', ()=>{
-    users = users.filter(u=>u.id !== socket.id)
-    
+  socket.on('disconnect', () => {
+    users = users.filter(u => u.id !== socket.id)
+
     io.sockets.emit('userDisconnect', JSON.stringify({
       id: socket.id
     }))
